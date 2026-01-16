f4158439951c03c3c643f18c3f2e081c01a9ba43
puneet vats <vats.puneet@gmail.com>
2026-01-16 13:15:59 +0800

    compute class

--- computeclass-cleanup.sh
+++ computeclass-cleanup.sh
@@ -5,3 +5,12 @@ CLUSTER_NAME="cc-cluster"
 ZONE="us-central1-a"
 
 gcloud container clusters delete $CLUSTER_NAME --zone $ZONE --quiet
+
+
+
+#gcloud container clusters update cc-cluster \
+#  --zone us-central1-a \
+#  --autoscaling-profile optimize-utilization
+
+
+# Flow is as follows : create cluster, create compute class, deploy normal pod, deploy pod with special hardware need, wait till cluster get new node. then delete deployment with special hardware requiremment, issue optimize cluster command - it should remove special need node from cluster. 
